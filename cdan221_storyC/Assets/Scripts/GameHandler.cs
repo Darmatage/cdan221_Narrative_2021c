7534e9033895ea11a9bbb20d04231f6b57cb303e
spacepawtrol <79662048+spacepawtrol@users.noreply.github.com>
2021-03-11 14:39:04 -0500

    updated gamehandler

--- cdan221_storyC/Assets/Scripts/GameHandler.cs
+++ cdan221_storyC/Assets/Scripts/GameHandler.cs
@@ -35,6 +35,9 @@ public class GameHandler : MonoBehaviour{
         }
 
         public void QuitGame(){
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
                 Application.Quit();
-        }
+                #endif
 }
