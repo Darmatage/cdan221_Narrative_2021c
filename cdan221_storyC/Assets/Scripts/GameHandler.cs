8d137b0476fd8d7f530b4171e96d388c912e24cb
spacepawtrol <79662048+spacepawtrol@users.noreply.github.com>
2021-03-11 15:32:01 -0500

    wip of the pause menu and new menuart

--- cdan221_storyC/Assets/Scripts/GameHandler.cs
+++ cdan221_storyC/Assets/Scripts/GameHandler.cs
@@ -2,19 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameHandler : MonoBehaviour{
 
         public static int playerStat;
+		
         //public GameObject textGameObject;
-
+		public static bool GameisPaused = false;
+        public GameObject pauseMenuUI;
+        public AudioMixer mixer;
+        public static float volumeLevel = 1.0f;
+        private Slider sliderVolumeCtrl;
+		
         //void Start () { UpdateScore (); }
 
-        void Update(){
-                if (Input.GetKey("escape")){
-                        Application.Quit();
+    
+        void Awake(){
+                SetLevel (volumeLevel);
+                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+                if (sliderTemp != null){
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+                        sliderVolumeCtrl.value = volumeLevel;
                 }
         }
+
+        void Start(){
+                pauseMenuUI.SetActive(false);
+        }
+
+        void Update(){
+                if (Input.GetKeyDown(KeyCode.Escape)){
+                        if (GameisPaused){
+                                Resume();
+                        }
+                        else{
+                                Pause();
+                        }
+                }
+        }
+
+        void Pause(){
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                GameisPaused = true;
+        }
+
+        public void Resume(){
+                pauseMenuUI.SetActive(false);
+                Time.timeScale = 1f;
+                GameisPaused = false;
+        }
+
+        public void RestartGame(){
+                Time.timeScale = 1f;
+                //restart the game:
+                //SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+SceneManager.LoadScene ("MainMenu");
+        }
+
+
+        public void SetLevel (float sliderValue){
+                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+                volumeLevel = sliderValue;
+        }
+
 
         public void AddPlayerStat(int amount){
                 playerStat += amount;
@@ -28,10 +80,6 @@ public class GameHandler : MonoBehaviour{
 
         public void StartGame(){
                 SceneManager.LoadScene("Scene1a");
-        }
-
-        public void RestartGame(){
-                SceneManager.LoadScene("MainMenu");
         }
 
         public void QuitGame(){
